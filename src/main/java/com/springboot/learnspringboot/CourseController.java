1e4ce0ed8ab7896785cb9c605b60b20dc5d7f3ec
ynaaju <ynaaju@LIN-PG02HH89.corp.capgemini.com>
2023-11-22 14:37:22 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,23 @@
+package com.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	//courses
+	//course: id, name
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "in28minutes" ),
+				new Course(2, "Learn DevOps", "in 28minutes")
+				);
+	}
+
+}
